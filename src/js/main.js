5e6e770ac4268e117119d4a7ece0167ddf134c89
Dmitriy Volchenko <Sverxsro4nik@yandex.ru>
2019-05-26 22:52:05 +0500

    adaptive meny

--- src/js/main.js
+++ src/js/main.js
@@ -19,3 +19,28 @@ function slider() {
 }
 document.querySelector('.slider').onclick = slider;
 setInterval(slider, 5000);
+//боковое меню
+function navSlide() {
+    var burger = document.querySelector('.burger');
+    var nav = document.querySelector('.nav__links');
+    var navLinks = document.querySelectorAll('.nav__links li');
+    var firstPageOpacity = document.querySelector('.page_title');
+    var secondImgOpacity = document.querySelector('.img_second');
+    var secondSpanOpacity = document.querySelector('.second_span');
+    console.log(firstPageOpacity);
+
+    burger.addEventListener('click', function(){
+        nav.classList.toggle('nav__active');
+        burger.classList.toggle('toggle');
+        firstPageOpacity.classList.toggle('opacityFirstPage');
+        secondImgOpacity.classList.toggle('opacityFirstPage');
+        secondSpanOpacity.classList.toggle('opacityFirstPage');
+    });
+    //animate links
+    navLinks.forEach(function(link, index){
+        link.style.animation = 'navLinkFade 0.5s ease forwards ' + (index / 7 + 1) + 's';
+        // `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`
+    });
+    //burger animation
+}
+navSlide();
